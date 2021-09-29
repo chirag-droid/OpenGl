5cd5b1a7a080c3a9a7d4e386c6fd918a37c006ff
Chirag Singla <chirag.singla.pi@gmail.com>
2021-09-29 04:26:35 -0700

    Implement shader uniform functions

--- src/Shader.cpp
+++ src/Shader.cpp
@@ -1,46 +1,51 @@
 #include <string>
+#include <iostream>
 #include <fstream>
 #include <sstream>
 #include <glad/glad.h>
 
 #include "Shader.h"
 
-ShaderProgramSources ParseShader(const std::string& filePath) {
-    std::ifstream stream(filePath);
-
-    enum ShaderType {
-        NONE = -1, VERTEX, FRAGMENT
-    };
-
-    std::string line;
-    std::stringstream ss[2];
-    ShaderType type = ShaderType::NONE;
-
-    // If the file is not open {"", ""} is returned
-    if (!stream.is_open()) return {"", ""};
-
-    // Iterates over each line in the file
-    while(stream.good()) {
-        std::getline(stream, line);
-
-        if (line.find("#shader vertex") != std::string::npos)
-            type = ShaderType::VERTEX;
-        else if (line.find("#shader fragment") != std::string::npos)
-            type = ShaderType::FRAGMENT;
-        else
-            ss[(int)type] << line << "\n";
-    }
-
-    return { ss[0].str(), ss[1].str() };
-}
-
 Shader::Shader(const std::string& filePath) {
     ShaderProgramSources source = ParseShader(filePath);
     m_ID = CreateShader(source.VertexSource, source.FragmentSource);
 }
 
+// Shader destructor
 Shader::~Shader() {
     glDeleteProgram(m_ID);   
+}
+
+void Shader::SetUniform3f(const std::string& name, float v0, float v1, float v2) {
+    glUniform3f(GetUniformLocation(name), v0, v1, v2);
+}
+
+void Shader::SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3) {
+    glUniform4f(GetUniformLocation(name), v0, v1, v2, v3);
+}
+
+// Private function to get location of a uniform
+int Shader::GetUniformLocation(const std::string& name) {
+    // If name exist in location cache return location
+    if (m_UniformLocationCache.find(name) != m_UniformLocationCache.end())
+        return m_UniformLocationCache[name];
+
+    // If location doesn't exist in cache, get the location
+    int location = glGetUniformLocation(m_ID, name.c_str());
+    if (location == -1)
+        std::cout << "Warning: Uniform " << name << " doesn't exists." << std::endl;
+
+    // Now, push the location int the cache
+    m_UniformLocationCache[name] = location;
+    return location;
+}
+
+void Shader::Bind() {
+    glUseProgram(m_ID);
+}
+
+void Shader::Unbind() {
+    glUseProgram(0);
 }
 
 unsigned int Shader::CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
@@ -48,8 +53,11 @@ unsigned int Shader::CreateShader(const std::string& vertexShader, const std::st
     GLuint vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
     GLuint fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
 
+    // Attach the vertex and fragment shaders to the program
     glAttachShader(program, vs);
     glAttachShader(program, fs);
+
+    // Link and validate the program
     glLinkProgram(program);
     glValidateProgram(program);
 
@@ -71,10 +79,40 @@ unsigned int Shader::CompileShader(unsigned int type, const std::string& source)
     return id;
 }
 
-void Shader::Bind() {
-    glUseProgram(m_ID);
-}
+// ParseShader takes a path in argument and parses it and returns ShaderProgramSources
+ShaderProgramSources ParseShader(const std::string& filePath) {
+    // Open the file stream
+    std::ifstream stream(filePath);
 
-void Shader::Unbind() {
-    glUseProgram(0);
+    // Define the ShaderType enum
+    enum ShaderType {
+        NONE = -1, VERTEX, FRAGMENT
+    };
+
+    std::string line;
+    std::stringstream ss[2];
+    ShaderType type = ShaderType::NONE;
+
+    // If the file is not open {"", ""} is returned
+    if (!stream.is_open()) return {"", ""};
+
+    // Iterates over each line in the file
+    while(stream.good()) {
+        std::getline(stream, line);
+
+        // If #shader vertex is found, set type to vertex
+        // which pushes the upcoming lines to 0 index of ss
+        if (line.find("#shader vertex") != std::string::npos)
+            type = ShaderType::VERTEX;
+
+        // If #shader fragment is found, set type to fragment
+        else if (line.find("#shader fragment") != std::string::npos)
+            type = ShaderType::FRAGMENT;
+
+        // If the line is a normal line, just push it in ss with index type
+        else
+            ss[(int)type] << line << "\n";
+    }
+
+    return { ss[0].str(), ss[1].str() };
 }
--- src/Shader.h
+++ src/Shader.h
@@ -1,25 +1,34 @@
 #pragma once
 
 #include <string>
+#include <unordered_map>
 
 struct ShaderProgramSources {
     std::string VertexSource;
     std::string FragmentSource;
 };
 
-ShaderProgramSources ParseShader(const std::string& filePath);
-
 class Shader
 {
 private:
     unsigned int m_ID;
+
+    // Maps name of the uniform to its location(int)
+    std::unordered_map<std::string, int> m_UniformLocationCache;
 public:
     Shader(const std::string& filePath);
     ~Shader();
+
+    void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+    void SetUniform3f(const std::string& name, float v0, float v1, float v2);
 
     void Bind();
     void Unbind();
 private:
     unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
     unsigned int CompileShader(unsigned int type, const std::string& source);
+
+    int GetUniformLocation(const std::string& name);
 };
+
+ShaderProgramSources ParseShader(const std::string& filePath);
